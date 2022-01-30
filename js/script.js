8e5f41feab1d56b5e727643e5acc88236a08f387
CanezinBeto <albertocanezin@gmail.com>
2022-01-30 14:38:27 -0300

    tabnav transformado em class

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 import initAnimacaoScroll from './modules/animascroll.js'
 import ScrollSuave from './modules/scrollsuave.js'
-import initTabNav from './modules/tabnav.js'
+import TabNav from './modules/tabnav.js'
 import Accordion from './modules/accordion.js'
 import initModal from './modules/modal.js'
 import initTooltip from './modules/tooltip.js'
@@ -12,12 +12,16 @@ import initFetchBitcoin from './modules/fetch-bitcoin.js'
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]')
 const accordion = new Accordion('[data-anime="accordion"] dt')
+const tabNav = new TabNav(
+  '[data-tab="menu"] li',
+  '[data-tab="content"] section'
+)
 
+tabNav.init()
 accordion.init()
 scrollSuave.init()
 
 initAnimacaoScroll()
-initTabNav()
 initModal()
 initTooltip()
 initDropDownMenu()
