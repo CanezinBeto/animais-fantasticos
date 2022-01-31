bb1e6453e4fca2efdda8b95cb09d7794f979103c
CanezinBeto <albertocanezin@gmail.com>
2022-01-31 13:32:19 -0300

    fetch animais refatorado

--- js/modules/fetch-animais.js
+++ js/modules/fetch-animais.js
@@ -1,26 +1,35 @@
 import AnimaNumeros from './anima-numeros.js'
-export default function initFetchAnimais() {
-  async function fetchAnimais(url) {
+export default function fetchAnimais(url, target) {
+  const numerosGrid = document.querySelector(target)
+
+  // Preenche cada animal no dom
+  function preencherAnimais(animal) {
+    const divAnimal = creatAnimal(animal)
+    numerosGrid.appendChild(divAnimal)
+  }
+
+  // Anima os números de cada animal
+  function animaAnimaisNumeros() {
+    const animaNumeros = new AnimaNumeros('[data-numero]', '.numeros', 'ativo')
+    animaNumeros.init()
+  }
+
+  // Puxa os animais através de um arquivo json e cria cada animal utilizando a função create animal
+  async function criarAnimais() {
     try {
+      // Fetch e espera a resposta e transforma em json
       const animaisResponse = await fetch(url)
       const animaisJSON = await animaisResponse.json()
-      const numerosGrid = document.querySelector('.numeros-grid')
 
-      animaisJSON.forEach((animal) => {
-        const divAnimal = creatAnimal(animal)
-        numerosGrid.appendChild(divAnimal)
-      })
-      const animaNumeros = new AnimaNumeros(
-        '[data-numero]',
-        '.numeros',
-        'ativo'
-      )
-      animaNumeros.init()
+      // Após a transformação de json, ativa as funções para preencher e animar os números
+      animaisJSON.forEach((animal) => preencherAnimais(animal))
+      animaAnimaisNumeros()
     } catch (erro) {
       console.log(erro)
     }
   }
 
+  // Cria a div contendo informações com o total de animais
   function creatAnimal(animal) {
     const div = document.createElement('div')
     div.classList.add('numero-animal')
@@ -30,5 +39,5 @@ export default function initFetchAnimais() {
     return div
   }
 
-  fetchAnimais('../js/animaisapi.json')
+  return criarAnimais()
 }
